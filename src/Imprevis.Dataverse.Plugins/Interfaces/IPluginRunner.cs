ae707201c5555207841d0b2b405f849bee28e8a9
Bob Guidinger <bguidinger@gmail.com>
2026-02-11 07:37:33 -0600

    Added additional base plugin classes (#10)

--- src/Imprevis.Dataverse.Plugins/Interfaces/IPluginRunner.cs
+++ src/Imprevis.Dataverse.Plugins/Interfaces/IPluginRunner.cs
@@ -1,5 +1,7 @@
 namespace Imprevis.Dataverse.Plugins
 {
+    using Microsoft.Xrm.Sdk;
+
     /// <summary>
     /// Interface for a plugin runner.
     /// </summary>
@@ -9,5 +11,34 @@ namespace Imprevis.Dataverse.Plugins
         /// This method is called by the plugin to execute the plugin logic.
         /// </summary>
         void Execute();
+    }
+
+
+    /// <summary>
+    /// Interface for a plugin runner with request and response types.
+    /// </summary>
+    /// <typeparam name="TRequest">Type of the request object.</typeparam>
+    public interface IPluginRunner<in TRequest> : IPluginRunner where TRequest : OrganizationRequest
+    {
+        /// <summary>
+        /// This method is called by the plugin to execute the plugin logic.
+        /// </summary>
+        /// <param name="request">The request object.</param>
+        void Execute(TRequest request);
+    }
+
+    /// <summary>
+    /// Interface for a plugin runner with request and response types.
+    /// </summary>
+    /// <typeparam name="TRequest">Type of the request object.</typeparam>
+    /// <typeparam name="TResponse">Type of the response object.</typeparam>
+    public interface IPluginRunner<in TRequest, out TResponse> : IPluginRunner where TRequest : OrganizationRequest where TResponse : OrganizationResponse
+    {
+        /// <summary>
+        /// This method is called by the plugin to execute the plugin logic.
+        /// </summary>
+        /// <param name="request">The request object.</param>
+        /// <returns>The response object.</returns>
+        TResponse Execute(TRequest request);
     }
 }
