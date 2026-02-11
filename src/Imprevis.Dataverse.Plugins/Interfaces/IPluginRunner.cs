cd9f8c252f5f5322456ac40a13d1e22d08df5396
Bob Guidinger <bguidinger@gmail.com>
2026-02-11 12:10:15 -0600

    Updated base plugin classes (#13)

--- src/Imprevis.Dataverse.Plugins/Interfaces/IPluginRunner.cs
+++ src/Imprevis.Dataverse.Plugins/Interfaces/IPluginRunner.cs
@@ -13,12 +13,11 @@ public interface IPluginRunner
     void Execute();
 }
 
-
 /// <summary>
 /// Interface for a plugin runner with request and response types.
 /// </summary>
 /// <typeparam name="TRequest">Type of the request object.</typeparam>
-public interface IPluginRunner<in TRequest> : IPluginRunner where TRequest : OrganizationRequest
+public interface IPluginRunner<in TRequest> where TRequest : OrganizationRequest
 {
     /// <summary>
     /// This method is called by the plugin to execute the plugin logic.
@@ -32,7 +31,7 @@ public interface IPluginRunner<in TRequest> : IPluginRunner where TRequest : Org
 /// </summary>
 /// <typeparam name="TRequest">Type of the request object.</typeparam>
 /// <typeparam name="TResponse">Type of the response object.</typeparam>
-public interface IPluginRunner<in TRequest, out TResponse> : IPluginRunner where TRequest : OrganizationRequest where TResponse : OrganizationResponse
+public interface IPluginRunner<in TRequest, out TResponse> where TRequest : OrganizationRequest where TResponse : OrganizationResponse
 {
     /// <summary>
     /// This method is called by the plugin to execute the plugin logic.
